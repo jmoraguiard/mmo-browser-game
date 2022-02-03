29ea5839c4deff286de82f3079640b7a1803a199
Joan Mora <joan.mora@inflight-vr.com>
2022-02-03 22:54:28 +0100

    Add minimum connectivity between Server and Client

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const path = require('path');
+const io = require('socket.io')(server);
+
+app.use(
+  express.static( path.join(__dirname, '/static') )
+);
+
+io.on('connection', socket => {
+  console.log('Some client connected')
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, ()=> {
+  console.log('listening on: ', port)
+});
